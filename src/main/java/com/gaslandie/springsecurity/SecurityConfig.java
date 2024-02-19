a045840b5ac8795258fcd412906e09591fe9de3c
Gassama <gassma803@gmail.com>
2024-02-19 18:02:37 +0100

    correction plusieurs messages d'erreurs en cours

--- src/main/java/com/gaslandie/springsecurity/SecurityConfig.java
+++ src/main/java/com/gaslandie/springsecurity/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.gaslandie.springsecurity;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,7 +11,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeRequests(requests -> requests.anyRequest().permitAll());
     }
     
 }
--- src/main/java/com/gaslandie/springsecurity/entities/AppUser.java
+++ src/main/java/com/gaslandie/springsecurity/entities/AppUser.java
@@ -3,15 +3,18 @@ package com.gaslandie.springsecurity.entities;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
